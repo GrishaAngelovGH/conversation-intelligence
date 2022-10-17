69e8b6965f3f9c853bb8fd819723cc029a50a251
Grisha Angelov <grisha.angelov89@gmail.com>
2022-10-17 15:39:23 +0300

    Add border and shadow to StatisticCard

--- src/components/Pages/Home/StatisticCards/StatisticCard.js
+++ src/components/Pages/Home/StatisticCards/StatisticCard.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 const StatisticCard = ({ chart, description }) => (
-  <div className="row align-items-center">
+  <div className="row align-items-center mt-3 border border-2 rounded shadow">
     <div className="col-md-6">
       {chart}
     </div>
--- src/components/Pages/Home/StatisticCards/StatisticCards.js
+++ src/components/Pages/Home/StatisticCards/StatisticCards.js
@@ -2,22 +2,13 @@ import TrackedKeywordsCard from './TrackedKeywordsCard'
 import SentimentCard from './SentimentCard'
 
 const StatisticCards = () => (
-  <div className="row mt-3">
+  <div className="row mt-3 p-3">
     <div className="col-md-12">
       <h3 className="fw-bold">What are your customers talking about?</h3>
       <p>Understand what's happening on sales calls, so you can coachyour sellers better.</p>
 
-      <div className="row mt-3 border border-2 rounded shadow">
-        <div className="col-md-12">
-          <TrackedKeywordsCard />
-        </div>
-      </div>
-
-      <div className="row mt-3 border border-2 rounded shadow">
-        <div className="col-md-12">
-          <SentimentCard />
-        </div>
-      </div>
+      <TrackedKeywordsCard />
+      <SentimentCard />
     </div>
   </div>
 )
