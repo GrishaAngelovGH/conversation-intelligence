ff15c5873904db000d04f0d831740c4581ba1896
Grisha Angelov <grisha.angelov89@gmail.com>
2022-10-17 13:12:54 +0300

    Integrate UserGreet and HistoryRange in Home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,3 +1,22 @@
-const Home = () => (<div>HOME</div>)
+import UserGreet from './UserGreet'
+import HistoryRange from './HistoryRange'
+
+const Home = () => {
+  return (
+    <div className="row g-0 justify-content-center text-center">
+			<div className="col-md-11">
+
+			<div className="row g-0 p-3">
+				<div className="col-md-6">
+					<UserGreet username="James Smith" />
+				</div>
+				<div className="col-md-6">
+					<HistoryRange />
+				</div>
+			</div>			
+			</div>
+		</div>
+  )
+}
 
 export default Home
