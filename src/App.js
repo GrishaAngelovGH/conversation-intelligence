88236520847add3a0f14a4e5de3f21d67a7b4c42
Grisha Angelov <grisha.angelov89@gmail.com>
2022-10-17 11:33:21 +0300

    Integrate Layout with Header and Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Layout from './components/Layout'
+import Header from './components/Header'
+
 import Home from './components/Pages/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Layout header={<Header />} body={<Home />} />} />
       </Routes>
     </Router>
   )
