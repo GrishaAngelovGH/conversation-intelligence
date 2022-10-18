868daecd9e849db932f62e037ff585bf110f4bab
Grisha Angelov <grisha.angelov89@gmail.com>
2022-10-18 13:57:27 +0300

    Add whitespace to title in StatisticCards

--- src/components/Pages/Home/StatisticCards/StatisticCards.js
+++ src/components/Pages/Home/StatisticCards/StatisticCards.js
@@ -6,7 +6,7 @@ const StatisticCards = () => (
   <div className="row mt-3 p-3">
     <div className="col-md-12">
       <h3 className="fw-bold">What are your customers talking about?</h3>
-      <p>Understand what's happening on sales calls, so you can coachyour sellers better.</p>
+      <p>Understand what's happening on sales calls, so you can coach your sellers better.</p>
 
       <TrackedKeywordsCard />
       <SentimentCard />
