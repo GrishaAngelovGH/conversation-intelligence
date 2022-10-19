7d126c377c841975450c1d2428f23d0f1971a2e4
Grisha Angelov <grisha.angelov89@gmail.com>
2022-10-19 14:42:34 +0300

    Replace MenuItem with Link in SidebarLayout

--- src/components/Layout/SidebarLayout.js
+++ src/components/Layout/SidebarLayout.js
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types'
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import { Link } from 'react-router-dom'
+import { Sidebar, Menu } from 'react-pro-sidebar'
 
 const SidebarLayout = ({ children }) => (
   <div className="row g-0">
     <div className="col-md-2">
-      <Sidebar width='210px'>
+      <Sidebar width="210px">
         <Menu>
-          <MenuItem href='/' prefix={<span>Home</span>} icon={<i className="bi bi-house-door"></i>} />
-          <MenuItem href='/transcript-summary' prefix={<span>Transcript Summary</span>} icon={<i className="bi bi-card-list"></i>} />
-          <MenuItem href='/seller-details' prefix={<span>Seller Details</span>} icon={<i className="bi bi-person"></i>} />
+          <Link to="/" className="d-block border mt-1 ms-1 p-2 text-decoration-none bg-transparent text-primary rounded">
+            <i className="bi bi-house-door"></i> <span className="fw-bold">Home</span>
+          </Link>
+          <Link to="/transcript-summary" className="d-block border ms-1 p-2 text-decoration-none bg-transparent text-primary rounded">
+            <i className="bi bi-card-list"></i> <span className="fw-bold">Transcript Summary</span>
+          </Link>
+          <Link to="/seller-details" className="d-block border ms-1 p-2 text-decoration-none bg-transparent text-primary rounded">
+            <i className="bi bi-person"></i> <span className="fw-bold">Seller Details</span>
+          </Link>
         </Menu>
       </Sidebar>
     </div>
--- src/components/Layout/SidebarLayout.test.js
+++ src/components/Layout/SidebarLayout.test.js
@@ -1,15 +1,24 @@
 import { render } from '@testing-library/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { ProSidebarProvider } from 'react-pro-sidebar'
 import SidebarLayout from './SidebarLayout'
 
 test('should render component', () => {
   const container = render(
-    <ProSidebarProvider>
-      <SidebarLayout>
-        <div>content</div>
-      </SidebarLayout>
-    </ProSidebarProvider>
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={(
+            <ProSidebarProvider>
+              <SidebarLayout>
+                <div>content</div>
+              </SidebarLayout>
+            </ProSidebarProvider>
+          )} />
+      </Routes>
+    </Router>
   )
 
   expect(container).toMatchSnapshot()
