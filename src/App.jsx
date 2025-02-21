43e0e339769959817889cedc3f0992f20aecaf38
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-21 10:01:14 +0200

    Change the extension for components to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ProSidebarProvider } from 'react-pro-sidebar'
+
+import Layout, { SidebarLayout } from './components/Layout'
+import Header from './components/Header'
+
+import Home from './components/Pages/Home'
+import TranscriptSummary from './components/Pages/TranscriptSummary'
+import SellerDetails from './components/Pages/SellerDetails'
+
+const withLayout = Component => (
+  <ProSidebarProvider>
+    <Layout
+      header={<Header />}
+      body={(
+        <SidebarLayout>
+          <Component />
+        </SidebarLayout>
+      )}
+    />
+  </ProSidebarProvider>
+)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={withLayout(Home)} />
+        <Route path='/transcript-summary' element={withLayout(TranscriptSummary)} />
+        <Route path='/seller-details' element={withLayout(SellerDetails)} />
+      </Routes>
+    </Router>
   )
 }
 
