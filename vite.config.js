7be50e220f90b7f758713b6d3222f3ae0bd32c6c
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-21 10:10:08 +0200

    Integrate vitest and generate all snapshots

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: "/src/components"
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.js'
+  }
 })
