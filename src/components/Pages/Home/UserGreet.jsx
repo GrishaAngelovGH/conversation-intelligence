11a97274c1c0756225a8955b34f15617c4af51ae
Grisha Angelov <grisha.angelov89@gmail.com>
2025-11-12 10:41:21 +0300

    Provide gradient colors for the user greet message

--- src/components/Pages/Home/UserGreet.jsx
+++ src/components/Pages/Home/UserGreet.jsx
@@ -1,5 +1,7 @@
 const UserGreet = ({ username }) => (
-  <h3 className='font-bold'>Good morning, {username}</h3>
+  <h3 className="text-4xl font-extrabold text-transparent bg-clip-text bg-linear-to-r from-blue-700 to-purple-500">
+    Good morning, {username}
+  </h3>
 )
 
 export default UserGreet
