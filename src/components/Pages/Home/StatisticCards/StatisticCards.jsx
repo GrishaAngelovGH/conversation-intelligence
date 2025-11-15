07bd649a03e76686c894303bb6d860a5d1b19766
Grisha Angelov <grisha.angelov89@gmail.com>
2025-11-15 14:12:07 +0300

    Improve StatisticCards section header with more distinct styling

--- src/components/Pages/Home/StatisticCards/StatisticCards.jsx
+++ src/components/Pages/Home/StatisticCards/StatisticCards.jsx
@@ -3,10 +3,14 @@ import SentimentCard from './SentimentCard'
 import BrandsCard from './BrandsCard'
 
 const StatisticCards = () => (
-  <div className='flex mt-3 p-3'>
-    <div className='w-full'>
-      <h3 className='font-bold'>What are your customers talking about?</h3>
-      <p>Understand what&apos;s happening on sales calls, so you can coach your sellers better.</p>
+  <div className='flex flex-col items-center mt-8 p-3'>
+    <div className='w-full text-center'>
+      <h3 className='text-4xl font-extrabold text-blue-800 text-center pb-3 mb-2'>
+        What are your customers talking about?
+      </h3>
+      <p className='text-2xl text-gray-500 mb-8'>
+        Understand what&apos;s happening on sales calls, so you can coach your sellers better.
+      </p>
 
       <TrackedKeywordsCard />
       <SentimentCard />
